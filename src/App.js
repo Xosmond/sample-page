1c6fcdf931ac51dd5a75c3af7b3d6df9172c92f1
Jordano Moscoso <cesar_notes@outlook.com>
2022-02-06 21:16:59 -0500

    temo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React UPDATED
+          Learn React UPDATED V3
         </a>
       </header>
     </div>
